e3c351f7a722a1f75efa7c41b58112eeb51417ef
Rohan Rangray <rrangray@ucsd.edu>
2016-04-24 15:33:46 -0700

    Removed unnecessary information from youtube.py's output

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -31,11 +31,14 @@ def get_video_description(vid_id, api_key):
     likes = j['statistics'].get('likeCount', 0)
     dislikes = j['statistics'].get('dislikeCount', 0)
     
+#    out = (u'\x02{snippet[title]}\x02 - length \x02{duration}\x02 - '
+#           u'{likes}\u2191{dislikes}\u2193 - '
+#           u'\x02{views}\x02 views - '
+#           u'\x02{snippet[channelTitle]}\x02 on \x02{published}\x02'
+#          ).format(duration=duration, likes=likes, dislikes=dislikes, views=views, published=published, **j)
     out = (u'\x02{snippet[title]}\x02 - length \x02{duration}\x02 - '
-           u'{likes}\u2191{dislikes}\u2193 - '
-           u'\x02{views}\x02 views - '
-           u'\x02{snippet[channelTitle]}\x02 on \x02{published}\x02'
-          ).format(duration=duration, likes=likes, dislikes=dislikes, views=views, published=published, **j)
+           u'\x02{snippet[channelTitle]}\x02'
+          ).format(duration=duration, **j)
 
     # TODO: figure out how to detect NSFW videos
 
